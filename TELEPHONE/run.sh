68c681fdd7f21518297db7046eb2f5f9b952e939
Abdullah <abdullahsaei@hotmail.com>
2023-09-15 18:05:37 +0200

    init commit telephone

---
+++ TELEPHONE/run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+pkill python
+
+# cd to this script dir
+# fixes activate venv from crontab
+# shellcheck disable=SC2164
+cd "${0%/*}"
+source venv/bin/activate
+
+export DISPLAY=:0.0
+xhost +
+
+# For clean start
+# Kill all relevant programs
+sudo pkill -9 -f telephone.py
+python3 src/telephone.py -c hh
+
